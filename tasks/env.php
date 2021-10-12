5f0e39a2a5a941fd28cedb3a1c7421ea7f6bf43d
Vu Thai Hoc <vuthaihoc.ttud@gmail.com>
2021-10-12 17:51:08 +0700

    change the way to use upload task

--- tasks/env.php
+++ tasks/env.php
@@ -18,21 +18,23 @@ task('upload_settings', function (){
     upload(base_dir('domains/{{domain}}/.env'), '{{release_path}}/.env');
     $domain = get('domain');
     $up_dir = base_dir('domains/' . $domain . '/disks');
-    $files = glob($up_dir . "/*.php");
-    foreach ($files as $file){
-        $remote_file_name = basename($file);
-        upload($file,  '{{release_path}}/disks/' . $remote_file_name);
-    }
+    upload($up_dir,  '{{release_path}}/disks/', [
+        'options' => [
+            "--include='*.php'",
+            "--exclude='*'",
+        ]
+    ]);
 });
 task('update_settings', function (){
     upload(base_dir('domains/{{domain}}/.env'), '{{current_path}}/.env');
     $domain = get('domain');
     $up_dir = base_dir('domains/' . $domain . '/disks');
-    $files = glob($up_dir . "/*.php");
-    foreach ($files as $file){
-        $remote_file_name = basename($file);
-        upload($file,  '{{current_path}}/disks/' . $remote_file_name);
-    }
+    upload($up_dir,  '{{current_path}}/disks/', [
+        'options' => [
+            "--include='*.php'",
+            "--exclude='*'",
+        ]
+    ]);
     $output = run('{{bin/php}} {{current_path}}/artisan config:cache');
     writeln('<info>' . $output . '</info>');
 });
